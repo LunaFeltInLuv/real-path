8d05ed3523c35266de24ccff1db675788d2325e4
Walter <146365467+LunaFeltInLuv@users.noreply.github.com>
2026-07-19 09:43:54 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
-import app from "../server";
+// Bắt buộc khai báo đuôi .js để Node.js trên Vercel có thể phân giải mô-đun chuẩn xác
+import app from "../server.js";
 import type { Request, Response } from "express";
 
 export default function handler(req: Request, res: Response) {
